b878d5b467aba65bc3e6db773f01954dcfb98ef2
jrankin2@my.wctc.edu <>
2013-01-24 17:17:11 -0600

    added "Stuff"

--- src/firstgit/FirstGit.java
+++ src/firstgit/FirstGit.java
@@ -6,5 +6,6 @@ public class FirstGit {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Good Bye");
+        System.out.println("Stuff");
     }
 }
